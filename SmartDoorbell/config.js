4df975f14cdf97fce9b9963c5618f780c89893d3
Singh-Abhishek-github <112816400+Singh-Abhishek-github@users.noreply.github.com>
2022-09-14 23:54:58 +0530

    First commit

---
+++ SmartDoorbell/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCT6vGzKb2HLI6flVSEl_CO3wiMCqZYhPs",
+    authDomain: "doorbell-63d4c.firebaseapp.com",
+    projectId: "doorbell-63d4c",
+    storageBucket: "doorbell-63d4c.appspot.com",
+    messagingSenderId: "177129045123",
+    appId: "1:177129045123:web:8b6be3761ce2ec4776afb6",
+    measurementId: "G-E47GD7S9FE"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
